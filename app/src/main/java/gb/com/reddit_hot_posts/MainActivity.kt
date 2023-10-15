d63db987bd13e552bf516faa1cf717fc1a2e9a71
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2023-10-15 15:44:03 +0500

    Initial commit

---
+++ app/src/main/java/gb/com/reddit_hot_posts/MainActivity.kt
@@ -0,0 +1,11 @@
+package gb.com.reddit_hot_posts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
